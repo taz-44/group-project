c16b62f9326cbdd22ce8c31670740f7585ce269c
Tyson <tyson.seow@gmail.com>
2018-06-27 10:28:16 -0600

    fixes again

--- src/Services/game-api.ts
+++ src/Services/game-api.ts
@@ -1,7 +1,7 @@
 import { Injectable } from "@angular/core";
 import { HttpClient, HttpHeaders } from "@angular/common/http";
 import { Observable } from "rxjs/Observable";
-import * as igdb from "../super-secure-data/igdb-key";
+import * as igdb from "../../secure-stuff/igdbKeys";
 
 
 @Injectable()
