f6948c25195b705b3ed5940419d1bf73b1ba40e8
Tyson <tyson.seow@gmail.com>
2018-07-03 08:09:11 -0600

    fixed firebase stuff

--- src/environment/environment.ts
+++ src/environment/environment.ts
@@ -3,7 +3,7 @@ import * as fireKey from "../../secure-stuff/firebaseKey"
 export const environment = {
   production: false,
   firebase: {
-    apiKey: fireKey,
+    apiKey: fireKey.default,
     authDomain: "mtech-group-4.firebaseapp.com",
     databaseURL: "https://mtech-group-4.firebaseio.com",
     projectId: "mtech-group-4",
